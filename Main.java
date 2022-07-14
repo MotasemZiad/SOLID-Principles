41ed507942f9298bd201cd63a2c749224d3f2821
MotasemZiad <miziad2000@gmail.com>
2022-07-14 11:19:40 +0300

    DIP initial

--- Main.java
+++ Main.java
@@ -71,23 +71,37 @@ public class Main {
      * implementation of low level classes but depends on abstraction.
      */
 
-    // 1. Single Responsibility Principle (S.R.P)
-    // Helps us with Code Reusability.
-    // It should be executed on both classes and methods.
+    /*
+     * 1. Single Responsibility Principle (S.R.P)
+     * Helps us with Code Reusability.
+     * It should be executed on both classes and methods.
+     * 
+     * 2. Open/Closed Principle (O.C.P)
+     * Any new functionality should be done by adding new classes instead of
+     * changing existing one.
+     * How to implement this?
+     * a. By adding new functionality to derived classes.
+     * b. Allow client access the original class with an abstract interface.
+     * 
+     * 3. Liskov Substitution Principle (L.S.P)
+     * "If you have class B inherits from class A then class A should be replaceable
+     * by class B without any changes"
+     * 
+     * 4. Interface Segregation (I.S.P)
+     * "Clients should not be forced to depend on methods they do not use"
+     * a. Avoid fat interface
+     * b. Client must not implement unnecessary methods
+     * 
+     * 5. Dependency Inversion Principle (D.I.P)
+     * "High level modules should not depend upon low level modules.
+     * Both should depend upon abstraction"
+     * We should not depend on concrete classes. we should depend on abstract
+     * classes.
+     * Tightly Coupling VS. Loosely Coupling
+     * Coupling VS. Cohesion
+     * Coupling describes the relationships between modules, and Cohesion describes
+     * the relationships within them.
+     * Usually low coupling and high cohesion is good for software.
+     */
 
-    // 2. Open/Closed Principle (O.C.P)
-    // Any new functionality should be done by adding new classes instead of
-    // changing existing one.
-    // How to implement this?
-    // a. By adding new functionality to derived classes.
-    // b. Allow client access the original class with an abstract interface.
-
-    // 3. Liskov Substitution Principle (L.S.P)
-    // "If you have class B inherits from class A then class A should be replaceable
-    // by class B without any changes"
-
-    // 4. Interface Segregation (I.S.P)
-    // "Clients should not be forced to depend on methods they do not use"
-    // a. Avoid fat interface
-    // b. Client must not implement unnecessary methods
 }
